328915af33b8fd154f25f2ca49c72211e835e75e
LNJAD <94455263+LNJAD@users.noreply.github.com>
2024-03-15 15:25:26 +0100

    Create ping.sh
    used in the cron

---
+++ ping.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ip_address=$1  # Adresse IP à surveiller
+email=$2
+lock_file="/home/pi/lock_file/$ip_address"  # Fichier de verrouillage
+
+if ping -c 1 "$ip_address" > /dev/null 2>&1; then
+    # L'adresse IP répond
+    if [ -f "$lock_file" ]; then
+        # Supprimer le fichier de verrouillage si l'adresse IP répond maintenant
+        rm "$lock_file"
+    fi
+    echo "L'adresse IP $ip_address répond."
+else
+    # L'adresse IP ne répond pas
+    if [ ! -f "$lock_file" ]; then
+        # Envoyer le flag seulement si le fichier de verrouillage n'existe pas
+        echo "L'adresse IP $ip_address ne répond pas."
+        # on envoie un mail 
+    echo "the camera is not reachable ip:$ip_address" | mail -s "camera out" "$email"    
+        touch "$lock_file"  # Créer un fichier de verrouillage
+    fi
+fi
